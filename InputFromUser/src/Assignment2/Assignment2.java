daaedb979a7a3cb65a458ec382522951b59603b2
Abhishek <Abhishek@DESKTOP-QIVS8ML>
2023-04-27 15:57:07 +0530

    New Changes

--- InputFromUser/src/Assignment2/Assignment2.java
+++ InputFromUser/src/Assignment2/Assignment2.java
@@ -34,21 +34,21 @@ public class Assignment2 {
     	//(((((x1*x2)-x3)+x4)-x5)/x6)
 		Assignment2 a = new Assignment2();
 		Scanner s = new Scanner(System.in);
-		System.out.println("Enter Value For Mul");
+		System.out.println("Enter Value For Multi");
 		int a1 = s.nextInt();
-		System.out.println("Enter 2nd value for Mul");
+		System.out.println("Enter 2nd value for Multi");
 		int a2 = s.nextInt();
 	    int a3 = a.mul(a1, a2);
-	    System.out.println("Enter value for Sub");
+	    System.out.println("Enter value for Subtract");
 		int a4 = s.nextInt();
         int a5 = a.Sub(a3,a4);
-        System.out.println("Enter Value for Sum");
+        System.out.println("Enter Value for Addition");
         int a6 = s.nextInt();
         int a7 = a.sum(a6, a5);
-        System.out.println("Enter Value for Sub");
+        System.out.println("Enter Value for Subtract");
         int a8 = s.nextInt();
         int a9 = a.Sub(a7, a8);
-        System.out.println("Enter Value for Dev");
+        System.out.println("Enter Value for Devide");
         float a10 = s.nextFloat();
         a.dev(a9, a10);
 	}
