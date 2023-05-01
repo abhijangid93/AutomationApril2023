cbc417a1538db1ee919928ece55f68858b250b7f
Abhishek <Abhishek@DESKTOP-QIVS8ML>
2023-05-02 01:49:42 +0530

    CheckPrimeNumber

---
+++ ConditionCheck/src/Prime/prime.java
@@ -0,0 +1,19 @@
+package Prime;
+import java.util.Scanner;
+
+public class prime {
+	
+	public static void main(String[] args) 
+	{
+		prime p = new prime();
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter Value ");
+		int prime = s.nextInt();
+		
+		{
+			
+		}
+		
+	}
+
+}
