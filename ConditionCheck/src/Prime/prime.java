fb233faac4e818288dc318a0953eb1c64be1ba2c
Abhishek <Abhishek@DESKTOP-QIVS8ML>
2023-05-04 01:12:26 +0530

    PrimeNumber

--- ConditionCheck/src/Prime/prime.java
+++ ConditionCheck/src/Prime/prime.java
@@ -7,12 +7,27 @@ public class prime {
 	{
 		prime p = new prime();
 		Scanner s = new Scanner(System.in);
-		System.out.println("Enter Value ");
+		System.out.println("Enter Number ");
 		int prime = s.nextInt();
-		
+		int count = 0;
+		for (int i=1;i<=prime;i++)
 		{
+			if(prime%i==0)
 			
+			count++;
 		}
+		
+			
+			if(count==2)
+		
+				System.out.println("The Number is Prime");
+		
+			else 
+			
+				System.out.println("Not Prime Number");
+			
+			
+		
 		
 	}
 
