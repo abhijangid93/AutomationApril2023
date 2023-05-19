a2da59679dfc3f5b09b2f04ffc7e12dc74609d5f
Abhishek <Abhishek@DESKTOP-QIVS8ML>
2023-05-20 00:05:29 +0530

    Factorial

---
+++ ConditionCheck/src/Factorial/Factorial.java
@@ -0,0 +1,24 @@
+package Factorial;
+import java.util.Scanner;
+import java.util.concurrent.SynchronousQueue;
+
+public class Factorial {
+	
+	//n=n* (n-1)
+	public static void main(String[] args) {
+			
+	Factorial f = new Factorial();
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter Your Value");
+	int n = 4;
+	int fact = 1;
+	int i;
+	for(i=1;i<=n;i++)
+	{
+		fact= fact * i;
+		
+	}
+	System.out.println("Factorial of Given Number is "+ fact);
+	}
+}
+
