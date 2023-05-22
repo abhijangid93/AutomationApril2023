5bc72c09f5fd57c5997c338469f7b57eb60f46c1
Abhishek <Abhishek@DESKTOP-QIVS8ML>
2023-05-23 00:23:24 +0530

    Startprint

---
+++ DoubleLoop/src/StarPrint/star.java
@@ -0,0 +1,19 @@
+package StarPrint;
+
+public class star {
+
+	public static void main(String[] args) 
+	{
+		int i,j;
+		for(i=1;i<=5;i=i+1)
+		{
+			for(j=1;j<=i;j=j+1)
+			{
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		
+		}
+	}
+}
