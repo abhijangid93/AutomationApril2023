055ba3dda001af8811a31fc1f04e91c1f633763a
Abhishek <Abhishek@DESKTOP-QIVS8ML>
2023-05-23 00:30:10 +0530

    DoubleStart

---
+++ DoubleLoop/src/StarPrint/Doublestartprint.java
@@ -0,0 +1,16 @@
+package StarPrint;
+
+public class Doublestartprint {
+	public static void main(String[] args) 
+	{
+		for(int i=5;i>=1;i=i-1)
+		{
+			for(int j=1;j<=i;j=j+1)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
