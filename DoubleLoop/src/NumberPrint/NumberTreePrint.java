d7809c3a8e2216c6ba0a8002e742fda653a236f3
Abhishek <Abhishek@DESKTOP-QIVS8ML>
2023-05-23 00:34:04 +0530

    NumberTreePrint

---
+++ DoubleLoop/src/NumberPrint/NumberTreePrint.java
@@ -0,0 +1,15 @@
+package NumberPrint;
+
+public class NumberTreePrint {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i=i+1)
+		{
+			for(int j=1;j<=i;j=j+1)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+}
