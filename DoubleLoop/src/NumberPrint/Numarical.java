7c251847e742e595c84bbae7ef6014c3e16eee43
Abhishek <Abhishek@DESKTOP-QIVS8ML>
2023-05-23 01:02:45 +0530

    TrianglePattern

---
+++ DoubleLoop/src/NumberPrint/Numarical.java
@@ -0,0 +1,18 @@
+package NumberPrint;
+
+public class Numarical {
+	public static void main(String[] args) {
+		{
+			int i,j,k=1	;
+			for(i=1;i<=4;i=i+1)
+			{
+				for(j=1;j<=i;j=j+1)
+				{
+					System.out.print(" "+ k++);
+				}		
+				System.out.println();
+			}
+		}
+	}
+
+}
