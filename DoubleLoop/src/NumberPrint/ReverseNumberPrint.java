aeeab94adbb3e34dbc81e9cc33718cee8237e280
Abhishek <Abhishek@DESKTOP-QIVS8ML>
2023-05-23 00:42:22 +0530

    NumberSpace

---
+++ DoubleLoop/src/NumberPrint/ReverseNumberPrint.java
@@ -0,0 +1,17 @@
+package NumberPrint;
+
+public class ReverseNumberPrint 
+ {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i=i+1)
+		{
+			for(int j=1;j<=i;j=j+1)
+			{
+				System.out.print(" "+j);
+			}
+			System.out.println();
+		}
+	}
+
+}
+
